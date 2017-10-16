e4836b933d906278ce859eae564d490035bae764
Kuba Neumann <kuba.neumann@gmail.com>
2017-10-16 11:26:55 +0200

    Run db scripts from main folder and gitignore typos

--- db/runHSQLDBClient.sh
+++ db/runHSQLDBClient.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp ./scripts/hsql-2.4.0.jar  org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost/workdb
+java -cp ./db/hsql-2.4.0.jar  org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost/workdb
--- db/runHSQLDBServer.sh
+++ db/runHSQLDBServer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -cp ./scripts/hsql-2.4.0.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 workdb
+java -cp ./db/hsql-2.4.0.jar org.hsqldb.server.Server --database.0 mem:mydb --dbname.0 workdb
